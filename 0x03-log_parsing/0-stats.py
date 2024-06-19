2a50875bc4553b871c66182cbfca21fbb1f14835
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-06-19 05:06:00 +0400

    another way

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,39 @@
 #!/usr/bin/python3
-"""
-Module contains the method print_stats that parse the log
-"""
 
-import re
-import signal
 import sys
 from collections import defaultdict
 
-total_size = 0
+
+def print_stats(status_codes, total_size):
+    """
+    Function to print the statistics
+    Args:
+        status_codes: dict of status codes
+        total_size: total size of the files
+    """
+    print(f"File size: {total_size}")
+    for code, count in sorted(status_codes.items()):
+        if count > 0:
+            print(f"{code}: {count}")
+
+
 status_codes = defaultdict(int)
-line_count = 0
+total_size = 0
+counter = 0
 
-# Regular expression pattern for the log lines
-log_pattern = re.compile(
-    r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - "
-    r'\[(.*?)\] "GET /projects/260 HTTP/1\.1" '
-    r"(\d{3}) (\d+)"
-)
-
-
-def print_stats(signum=None, frame=None):
-    """Function to print the statistics"""
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-    if signum is not None:
-        sys.exit()
-
-
-# Set up signal handling for CTRL+C
-signal.signal(signal.SIGINT, print_stats)
-
-# Read from stdin line by line
 try:
     for line in sys.stdin:
-        line_count += 1
-        match = log_pattern.match(line)
-        if match:
-            total_size += int(match.group(4))
-            status_codes[match.group(3)] += 1
+        parsed_line = line.split()
+        if len(parsed_line) > 2:
+            counter += 1
+            total_size += int(parsed_line[-1])  # file size
+            code = parsed_line[-2]  # status code
 
-        if line_count % 10 == 0:
-            print_stats()
-    print_stats()
-except KeyboardInterrupt:
-    print_stats()
-    raise
-# except Exception as e:
-#    print(f"Error: {e}", file=sys.stderr)
+            if code in status_codes:
+                status_codes[code] += 1
 
-# Print final statistics if not exiting due to CTRL+C
-# print_stats()
+            if counter % 10 == 0:
+                print_stats(status_codes, total_size)
+
+finally:
+    print_stats(status_codes, total_size)
---
+++ 0x03-log_parsing/10-stats.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""
+Module contains the method print_stats that parse the log
+"""
+
+import re
+import signal
+import sys
+from collections import defaultdict
+
+total_size = 0
+status_codes = defaultdict(int)
+line_count = 0
+
+# Regular expression pattern for the log lines
+log_pattern = re.compile(
+    r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - "
+    r'\[(.*?)\] "GET /projects/260 HTTP/1\.1" '
+    r"(\d{3}) (\d+)"
+)
+
+
+def print_stats(signum=None, frame=None):
+    """Function to print the statistics"""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
+    if signum is not None:
+        sys.exit()
+
+
+# Set up signal handling for CTRL+C
+signal.signal(signal.SIGINT, print_stats)
+
+# Read from stdin line by line
+try:
+    for line in sys.stdin:
+        line_count += 1
+        match = log_pattern.match(line)
+        if match:
+            total_size += int(match.group(4))
+            status_codes[match.group(3)] += 1
+
+        if line_count % 10 == 0:
+            print_stats()
+    print_stats()
+except KeyboardInterrupt:
+    print_stats()
+    raise
+# except Exception as e:
+#    print(f"Error: {e}", file=sys.stderr)
+
+# Print final statistics if not exiting due to CTRL+C
+# print_stats()
