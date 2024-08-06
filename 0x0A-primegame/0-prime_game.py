3c591d88169d8afacc7740c1bb0a8aaa487d0395
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-08-06 21:52:30 +0400

    0. Prime Game - debug02

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -15,51 +15,29 @@ def isWinner(x, nums):
     Returns:
         str: The name of the winner ('Maria' or 'Ben') or None if it's a tie.
     """
-
-    def sieve_of_eratosthenes(n):
-        """
-        Return a list of primes up to n using the Sieve of Eratosthenes
-        algorithm.
-
-        Args:
-            n (int): The upper limit to find primes.
-
-        Returns:
-            list: A list of prime numbers up to n.
-        """
-        is_prime = [True] * (n + 1)
-        p = 2
-        while p * p <= n:
-            if is_prime[p]:
-                for i in range(p * p, n + 1, p):
-                    is_prime[i] = False
-            p += 1
-        return [p for p in range(2, n + 1) if is_prime[p]]
-
-    def determine_winner(n):
-        """
-        Determine the winner of the game given n.
-
-        Args:
-            n (int): The upper limit to find primes.
-
-        Returns:
-            str: The name of the winner ('Maria' or 'Ben').
-        """
-        if n < 2:
-            return "Ben"
-        primes = sieve_of_eratosthenes(n)
-        # Game strategy based on number of primes
-        return "Maria" if len(primes) % 2 == 1 else "Ben"
-
-    win_count = {"Maria": 0, "Ben": 0}
-    for num in nums:
-        winner = determine_winner(num)
-        win_count[winner] += 1
-
-    if win_count["Maria"] > win_count["Ben"]:
-        return "Maria"
-    elif win_count["Ben"] > win_count["Maria"]:
-        return "Ben"
-    else:
+    if x <= 0 or nums is None or x != len(nums):
         return None
+
+    max_n = max(nums)
+    is_prime = [True] * (max_n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(max_n**0.5) + 1):
+        if is_prime[i]:
+            for multiple in range(i * i, max_n + 1, i):
+                is_prime[multiple] = False
+
+    scores = {"Maria": 0, "Ben": 0}
+
+    for n in nums:
+        prime_count = sum(is_prime[: n + 1])
+        if prime_count % 2 == 0:
+            scores["Ben"] += 1
+        else:
+            scores["Maria"] += 1
+
+    if scores["Maria"] > scores["Ben"]:
+        return "Maria"
+    if scores["Ben"] > scores["Maria"]:
+        return "Ben"
+    return None
